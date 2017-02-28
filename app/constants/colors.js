f16a929ad8093460d7891bcf5601b8d547604412
Oyvind Moldestad <moldestad@gmail.com>
2017-02-28 17:24:51 +0100

    Legg til farger

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,4 +1,15 @@
 export default {
-  DARK_BLUE: '#517fa4',
-  WHITE: '#fff'
+  PURPLE: '#9c26b0',
+  LIGHT_BLUE: '#2095f3',
+  DARK_BLUE: '#4a6ca7',
+  DARK_GREEN: '#009588',
+  LIGHT_GREEN: '#8ac24a',
+  YELLOW: '#ffc108',
+  ORANGE: '#ff5722',
+  RED: '#cd1f03',
+  PINK: '#e81e63',
+  LIGHT_GRAY: '#eef0f3',
+  DARK_GRAY: '#9e9e9e',
+  WHITE: '#fff',
+  BLACK: '#000'
 }
