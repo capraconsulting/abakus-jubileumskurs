2d23e90bdc0c973db4821714603779faf45ab969
Oyvind Moldestad <moldestad@gmail.com>
2017-02-28 17:44:39 +0100

    Legg til icons

--- app/constants/icons.js
+++ app/constants/icons.js
@@ -1,3 +1,11 @@
+const FONT_AWESOME = 'font-awesome';
+
 export default {
-  BEER: {name: 'beer', type: 'font-awesome'}
+  AMBULANCE: {name: 'ambulance', type: FONT_AWESOME},
+  BEER: {name: 'beer', type: FONT_AWESOME},
+  CAMERA: {name: 'camera', type: FONT_AWESOME},
+  LIST: {name: 'list', type: FONT_AWESOME},
+  MINUS: {name: 'minus', type: FONT_AWESOME},
+  PERCENT: {name: 'percent', type: FONT_AWESOME},
+  PLUS: {name: 'plus', type: FONT_AWESOME}
 }
