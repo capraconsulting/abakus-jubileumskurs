3649c42273848ef5ecfd1d664b122a1cd4cc7bb1
Oyvind Moldestad <moldestad@gmail.com>
2017-02-28 18:02:54 +0100

    Legg til initialstate

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -1,10 +1,33 @@
-// import { combineReducer } from 'redux';
-//
-// export default combineReducer({
-//
-// });
+const initialState = {
+  newBrew: {
+    brewName: '',
+    brewery: '',
+    alcohol: '',
+    brewType: '',
+    rating: '',
+    image: ''
+  },
+  brewList: [
+    {
+      brewName: 'Fatøl',
+      brewery: 'Hansa',
+      alcohol: 4.7,
+      brewType: 'Fatøl',
+      rating: 4,
+      image: 'http://beerbikemunich.com/demo/slider/01.jpg'
+    },
+    {
+      brewName: 'Pilsner',
+      brewery: 'Ringnes',
+      alcohol: 4.7,
+      brewType: 'Pilsner',
+      rating: 2,
+      image: 'http://beerbikemunich.com/demo/slider/01.jpg'
+    }
+  ]
+}
 
-export default function reducer(state = {}, action) {
+export default function reducer(state = initialState, action) {
   switch (action.type) {
     default:
       return state;
